3b582436fbfd328bf212d215c9b70b8e0970ae63
Seungwan Ko <mukho@kakao.com>
2022-12-11 23:15:16 +0900

    README.md 수정

--- src/components/views/Title.js
+++ src/components/views/Title.js
@@ -5,7 +5,7 @@ function Title(){
         <body>
             <div>
                 <a href="/">
-                    <h2>ESPers Starcraft System</h2>
+                    <h2>ESPers Starcraft Ranking System</h2>
                 </a>
             </div>
         </body>
