1b8f6cd533d1397cb8f176c91874ef6c60da4b76
Mukho <mukho@kakao.com>
2023-09-23 16:22:47 +0900

    리팩토링 백업

--- README.md
+++ README.md
@@ -6,15 +6,14 @@ ESPers Starcraft Ranking System's Frontend
 
 ![image](https://github.com/mukhoplus/rank-system_front/assets/67003627/bc3ac2c4-f9ae-44f9-a1f4-0c6f3c49c07e)
 
-
 ## Version
 
-1.4.4
+2.0.0
 
 ## How To Run
 
-1. ``git clone https://github.com/mukhoplus/rank-system.git``
-2. ``cd ./src/main/``
-3. ``git clone https://github.com/mukhoplus/rank-system_front.git frontend``
+1. `git clone https://github.com/mukhoplus/rank-system.git`
+2. `cd ./src/main/`
+3. `git clone https://github.com/mukhoplus/rank-system_front.git frontend`
 4. Set database tables.
 5. Run spring boot server.
--- src/App.js
+++ src/App.js
@@ -1,85 +1,83 @@
-import React from 'react';
-import{
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import NavBar from './components/views/NavBar';
-import Main from './components/views/Main';
-import SignUp from './components/views/SignUp';
-import Login from './components/views/Login';
-import Games from './components/views/Games';
-import AddGamer from './components/views/AddGamer';
-import AddGame from './components/views/AddGame';
-import NameRanking from './components/views/NameRanking';
-import Record from './components/views/Record';
-import ViewRecord from './components/views/ViewRecord';
-import Relative from './components/views/Relative';
-import ViewRelative from './components/views/ViewRelative';
-import Footer from './components/views/Footer';
+import NavBar from "./components/views/NavBar";
+import Main from "./components/views/Main";
+import SignUp from "./components/views/SignUp";
+import Login from "./components/views/Login";
+import Games from "./components/views/Games";
+import AddGamer from "./components/views/AddGamer";
+import AddGame from "./components/views/AddGame";
+import NameRanking from "./components/views/NameRanking";
+import Record from "./components/views/Record";
+import ViewRecord from "./components/views/ViewRecord";
+import Relative from "./components/views/Relative";
+import ViewRelative from "./components/views/ViewRelative";
+import Footer from "./components/views/Footer";
 
 function App() {
-    function getCookies(){
-        function isCookie(obj){
-            if(JSON.stringify(obj) === '[""]') return 0;
-            else return Object.keys(obj).length;
-        }
-
-        let cookie = {};
-        const allCookies = document.cookie.split('; ');
-        allCookies.forEach(c =>{
-            const temp = c.split('=');
-            cookie[temp[0]] = temp[1];
-        });
-
-        const id = isCookie(allCookies) ? cookie['id'] : "";
-        const name = isCookie(allCookies) ? cookie['name'] : "";
-        const permission = isCookie(allCookies) ? cookie['permission'] : "";
-
-        return [id, name, permission];
-    }
-    
-    let data = getCookies();
-    const permission = data[2];
-    
-    function routing(){
-        return (
-            <Router>
-                <body>
-                    <NavBar />
-
-                    <hr/>
-                        <div className="container">
-                            <Switch>
-                                <Route exact path="/" component={Main} />
-                                <Route path="/login" component={Login} />
-                                <Route path="/signup" component={SignUp} />
-                                <Route path="/games" component={Games} />
-                                <Route path="/addgamer" component={AddGamer} />
-                                <Route path="/addgame" component={AddGame} />
-                                <Route path="/nameranking" component={NameRanking} />
-                                <Route path="/record" component={Record} />
-                                <Route path="/viewrecord" component={ViewRecord} />
-                                <Route path="/relative" component={Relative} />
-                                <Route path="/viewrelative" component={ViewRelative} />
-                            </Switch>
-                        </div>
-                    <hr/>
-
-                    <Footer />
-                </body>
-            </Router>
-        );
+  function getCookies() {
+    function isCookie(obj) {
+      if (JSON.stringify(obj) === '[""]') return 0;
+      else return Object.keys(obj).length;
     }
 
-    if(['/addgamer', '/addgame'].indexOf(window.location.pathname) !== -1 && permission !== 'true'){
-        alert('권한이 없습니다.\n운영자에게 문의하세요.');
-        window.location.href = "/";
-    }
-    else{
-        return routing();
-    }
+    let cookie = {};
+    const allCookies = document.cookie.split("; ");
+    allCookies.forEach((c) => {
+      const temp = c.split("=");
+      cookie[temp[0]] = temp[1];
+    });
+
+    const id = isCookie(allCookies) ? cookie["id"] : "";
+    const name = isCookie(allCookies) ? cookie["name"] : "";
+    const permission = isCookie(allCookies) ? cookie["permission"] : "";
+
+    return [id, name, permission];
+  }
+
+  let data = getCookies();
+  const permission = data[2];
+
+  function routing() {
+    return (
+      <Router>
+        <body>
+          <NavBar />
+
+          <hr />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/games" component={Games} />
+              <Route path="/addgamer" component={AddGamer} />
+              <Route path="/addgame" component={AddGame} />
+              <Route path="/nameranking" component={NameRanking} />
+              <Route path="/record" component={Record} />
+              <Route path="/viewrecord" component={ViewRecord} />
+              <Route path="/relative" component={Relative} />
+              <Route path="/viewrelative" component={ViewRelative} />
+            </Switch>
+          </div>
+          <hr />
+
+          <Footer />
+        </body>
+      </Router>
+    );
+  }
+
+  if (
+    ["/addgamer", "/addgame"].indexOf(window.location.pathname) !== -1 &&
+    permission !== "true"
+  ) {
+    alert("권한이 없습니다.\n운영자에게 문의하세요.");
+    window.location.href = "/";
+  } else {
+    return routing();
+  }
 }
 
 export default App;
